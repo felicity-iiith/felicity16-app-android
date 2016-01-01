4ea7560c464fdbf257051f15d922d327fd0d46c8
mukul hase <mukulhase@gmail.com>
2016-01-01 23:26:41 +0530

    Cleaning Up, sample template, adding events view

--- www/js/services.js
+++ www/js/services.js
@@ -1,10 +1,10 @@
 angular.module('starter.services', [])
 
-.factory('Chats', function() {
+.factory('Events', function() {
   // Might use a resource here that returns a JSON array
 
   // Some fake testing data
-  var chats = [{
+  var events = [{
     id: 0,
     name: 'Ben Sparrow',
     lastText: 'You on your way?',
@@ -33,15 +33,15 @@ angular.module('starter.services', [])
 
   return {
     all: function() {
-      return chats;
+      return events;
     },
-    remove: function(chat) {
-      chats.splice(chats.indexOf(chat), 1);
+    remove: function(eventNumber) {
+      events.splice(events.indexOf(eventNumber), 1);
     },
-    get: function(chatId) {
+    get: function(eventNumber) {
       for (var i = 0; i < chats.length; i++) {
-        if (chats[i].id === parseInt(chatId)) {
-          return chats[i];
+        if (chats[i].id === parseInt(eventNumber)) {
+          return events[i];
         }
       }
       return null;
