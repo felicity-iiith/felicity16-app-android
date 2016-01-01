22bacdc3aa273d4c7574f4a81192f877c2ccf164
mukul hase <mukulhase@gmail.com>
2016-01-02 00:00:19 +0530

    Added service for storing events and their categories

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -2,9 +2,17 @@ angular.module('starter.controllers', [])
   .controller('HomeCtrl', function ($scope, $stateParams, Events) {
   })
   .controller('CategoryCtrl', function ($scope, $stateParams, Events) {
-    $scope.title = $stateParams.categoryid;
-    $scope.events = Events.all()
+    var category;
+    var events=Events.all();
+    for(var x in events.categories){
+      if(events.categories[x].id==$stateParams.categoryid){
+        category=events.categories[x];
+        break;
+      }
+    }
+    $scope.title = category.name;
+    $scope.events = category.events;
   })
-  .controller('EventsCtrl', function ($scope) {
-    $scope.categories = [{name: "hi", id: 1}, {name: "bye", id: 2}];
+  .controller('EventsCtrl', function ($scope, Events) {
+    $scope.categories = Events.all().categories;
   });
--- www/js/services.js
+++ www/js/services.js
@@ -2,34 +2,22 @@ angular.module('starter.services', [])
 
 .factory('Events', function() {
   // Might use a resource here that returns a JSON array
-
-  // Some fake testing data
-  var events = [{
-    id: 0,
-    name: 'Ben Sparrow',
-    lastText: 'You on your way?',
-    face: 'img/ben.png'
-  }, {
-    id: 1,
-    name: 'Max Lynx',
-    lastText: 'Hey, it\'s me',
-    face: 'img/max.png'
-  }, {
-    id: 2,
-    name: 'Adam Bradleyson',
-    lastText: 'I should buy a boat',
-    face: 'img/adam.jpg'
-  }, {
-    id: 3,
-    name: 'Perry Governor',
-    lastText: 'Look at my mukluks!',
-    face: 'img/perry.png'
-  }, {
-    id: 4,
-    name: 'Mike Harrington',
-    lastText: 'This is wicked good ice cream.',
-    face: 'img/mike.png'
-  }];
+  var events = {
+    categories:[
+      {
+        name:"Threads",
+        id:1,
+        events:[
+          {
+            name:"CodeCraft"
+          },
+          {
+            name:"Cache-In"
+          }
+        ]
+      }
+    ]
+  };
 
   return {
     all: function() {
