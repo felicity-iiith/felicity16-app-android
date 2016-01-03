5dccff6bb3081941c124a03f62513621cf4b38a4
harshil goel <harshil158@gmail.com>
2016-01-03 18:52:39 +0530

    Links are hard coded

--- www/js/services.js
+++ www/js/services.js
@@ -27,6 +27,54 @@ angular.module('starter.services', [])
             "event_venue": null // optional
           }
         ]
+      },
+      {
+        name:"Threads",
+        id:1,
+        events:[
+          {
+            "id":1,
+            "title": "CodeCraft", // page title
+            "tagline": "So, you think you can code?",
+            "long_description": "Lorem ipsum",
+            "event_start_time": 1451765833,
+            "event_end_time": 1451852247,
+            "event_venue": null // optional
+          },
+          {
+            "id":2,
+            "title": "Cache-In", // page title
+            "tagline": "So, you think you can craft?",
+            "long_description": "Lorem ipsum",
+            "event_start_time": 1451765833,
+            "event_end_time": 1451852247,
+            "event_venue": null // optional
+          }
+        ]
+      },
+      {
+        name:"Threads",
+        id:1,
+        events:[
+          {
+            "id":1,
+            "title": "CodeCraft", // page title
+            "tagline": "So, you think you can code?",
+            "long_description": "Lorem ipsum",
+            "event_start_time": 1451765833,
+            "event_end_time": 1451852247,
+            "event_venue": null // optional
+          },
+          {
+            "id":2,
+            "title": "Cache-In", // page title
+            "tagline": "So, you think you can craft?",
+            "long_description": "Lorem ipsum",
+            "event_start_time": 1451765833,
+            "event_end_time": 1451852247,
+            "event_venue": null // optional
+          }
+        ]
       }
     ]
   };
