863f22abcb59d271abbb23c3bbc17713f622691e
mukulhase <mukulhase@gmail.com>
2016-02-15 23:09:34 +0530

    Caching

--- www/js/services.js
+++ www/js/services.js
@@ -1,69 +1,107 @@
 angular.module('starter.services', [])
 
   .factory('Events', function ($http) {
-    var events={};
-    var categories={};
-    var lastupdated;
+    var events = {};
+    var categories = {};
+    var list = {};
+    var count = 0;
     var urls = {
-      events:"http://felicity.iiit.ac.in/api"
+      events: "http://felicity.iiit.ac.in/api"
     };
+    if ("events" in window.localStorage) {
+      events = JSON.parse(window.localStorage["events"]);
+    }else{
+      events = {};
+    }
+    if ("categories" in window.localStorage) {
+      categories = JSON.parse(window.localStorage["categories"]);
+    }else{
+      categories = {};
+    }
+    if ("list" in window.localStorage) {
+      list = JSON.parse(window.localStorage["list"]);
+    }else{
+      list = {};
+    }
     // Might use a resource here that returns a JSON array
     var updateFirst = function () {
-      return $http.get(urls.events).then(function (response) {
+      return $http.get(urls.events+"?prev_id="+list.version_id).then(function (response) {
         updateEvents(response);
-        updateTime(new Date());
         return events;
       });
     };
-    var updateTime = function(up){
-      lastupdated = up;
-      window.localStorage["lastupdated"] = up;
-    };
-    var addCategory =function(name,path){
-      $http.get(urls.events+"/"+path.split('/')[1]).then(function (response) {
-        categories[name]=response.data;
-        categories[name].path=path.split('/')[1];
+    var addCategory = function (name, path) {
+      var request;
+      if(name in categories){
+        request=$http.get(urls.events + "/" + path.split('/')[1]+"?prev_id="+categories[name].version_id);
+      }else{
+        request=$http.get(urls.events + "/" + path.split('/')[1]);
+      }
+      request.then(function (response) {
+        if (response.data != "") {
+          categories[name] = response.data;
+          console.log(categories);
+          categories[name].path = path.split('/')[1];
+        }
+        count--;
       });
     };
-    var addEvent =function(name,path){
-      $http.get(urls.events+path).then(function (response) {
-        events[name]=response.data;
-        events[name].category = path.split('/')[1];
+    var addEvent = function (name, path) {
+      var request;
+      if(name in events){
+        request=$http.get(urls.events + "/" + path+"?prev_id="+events[name].version_id);
+      }else{
+        request=$http.get(urls.events + "/" + path);
+      }
+      request.then(function (response) {
+        if (response.data != "") {
+          events[name] = response.data;
+          events[name].category = path.split('/')[1];
+        }
+        count--;
       });
+
     };
-    var updateEvents = function(up){
-      events = up.data.page_data.events_data;
+    var updateEvents = function (up) {
+      if (up.data != "") {
+        list = up.data;
+      }
+      var events_data = list.page_data.events_data;
       var i;
-      for(i in events){
-        if(events[i].template=="category"){
-          addCategory(events[i].data.name,events[i].path);
-        }else if(events[i].template=="event") {
-          addEvent(events[i].data.name,events[i].path);
+      count = 0;
+      for (i in events_data) {
+        if (events_data[i].template == "category") {
+          count++;
+          addCategory(events_data[i].data.name, events_data[i].path);
+        } else if (events_data[i].template == "event") {
+          count++;
+          addEvent(events_data[i].data.name, events_data[i].path);
         }
       }
-      window.localStorage["events"] = events;
+      save();
+      console.log(events);
     };
+
+    function save() {
+      if (count == 0) {
+        console.log("updated!!");
+        window.localStorage["events"] = JSON.stringify(events);
+        window.localStorage["categories"] = JSON.stringify(categories);
+        window.localStorage["list"] = JSON.stringify(list);
+      } else {
+        setTimeout(save, 50);
+      }
+    }
+
     updateFirst();
-/*
-    if ("lastupdated" in window.localStorage) {
-      lastupdated = window.localStorage["events"];
-      events = JSON.parse(window.localStorage["events"]);
-    }else {
-      updateFirst();
-    }
-    if ("events" in window.localStorage) {
-      events = JSON.parse(window.localStorage["events"]);
-    }else {
-      events ={};
-    }
-*/
+
     return {
       update: function () {
         //update here
 
       },
       "updateFirst": updateFirst,
-      categories: function() {
+      categories: function () {
         return categories;
       },
       category: function (category) {
